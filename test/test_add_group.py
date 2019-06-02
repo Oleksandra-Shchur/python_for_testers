03f18558b5553fc7f36f58fe98cdefd970cce859
oleksandra <oleksandra126@gmail.com>
2019-06-02 22:58:03 +0300

    grouped by packages

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,9 +1,8 @@
 # -+- coding: utf-8 -+-
-from selenium.webdriver.firefox.webdriver import WebDriver
 import pytest
 
-from application import Application
-from group import Group
+from fixture.application import Application
+from model.group import Group
 
 
 @pytest.fixture
