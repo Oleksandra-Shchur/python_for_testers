01d5d4189368698b1b9f9a0e7c9f5e077bdef3c7
oleksandra <oleksandra126@gmail.com>
2019-06-04 16:04:36 +0300

    randomization

--- fixture/group.py
+++ fixture/group.py
@@ -41,23 +41,33 @@ class GroupHelper:
             wd.find_element_by_name(field_name).clear()
             wd.find_element_by_name(field_name).send_keys(text)
 
-    def delete_first_group(self):
+    def delete_group_by_index(self, index):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # submit deletion
         wd.find.element_by_name("delete").click()
         self.return_to_group_page()
         self.group_cache = None
 
+    def delete_first_group(self):
+        self.delete_group_by_index(0)
+
     def select_first_group(self):
         wd = self.app.wd
         wd.find.element_by_name("selected[]").click()
 
-    def modify_first_group(self, new_group_data):
+    def select_group_by_index(self, index):
+        wd = self.app.wd
+        wd.find.elements_by_name("selected[]")[index].click()
+
+    def modify_first_group(self):
+        self.modify_group_by_index(0)
+
+    def modify_group_by_index(self, index, new_group_data):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # open modification form
         wd.find.element_by_name("edit").click()
         # fill group form
